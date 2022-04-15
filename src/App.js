c6d101ce714f8d2695d15a20dd7c88617f584b83
JordyAM <JAlexanderMcLean@Gmail.com>
2022-04-15 11:40:42 -0400

    last day, here we go

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link} from 'react-router-dom';
-import { useState, useEffect } from 'react/cjs/react.production.min';
-import { Socket } from 'socket.io-client';
-import TextField from '@mui/material'
+import { useState, useEffect } from 'react';
+// import { Socket } from 'socket.io-client';
+import { TextField } from '@mui/material'
 import io from 'socket.io-client';
 
-const socket = io.connect('https://rocky-peak-12032.herokuapp.com')
-
+const socket = io.connect('http://localhost:8000')
+// https://rocky-peak-12032.herokuapp.com' ||
 function App() {
   const [ comment, setComment] = useState({message: '', name:''})
+  const [chatLog, setChatLog] = useState([])
   const [chat, setChat] = useState([])
 
   useEffect(() => {
-    Socket.on('message', ({name,message}) => {
-      setChat([...chat, {name, message}])
+    socket.on('message', ({name,message}) => {
+      setChatLog([...chatLog, {name, message}])
     })
   })
   const onTextChange = e => {
-    setComment({...state, [e.target.name]: e.target.value})
+    setComment({...comment, [e.target.name]: e.target.value})
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const {name, message} = comment
-    Socket.emit('message', {name,message})
+    socket.emit('message', {name,message})
     setComment({message: '', name})
   }
-  const renderChat = () => {
-    return chat.map(({name, message}, index) => {
-      <div key={index}>
+  const renderChatLog = () => {
+    return chatLog.map(({name, message}, index) => {
+      return (<div key={index}>
         <h3>{name}: <span>{message}</span></h3>
-      </div>
+      </div>)
     })
   }
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <h1>Messenger</h1>
+        <h1>EtherBox</h1>
         <div className="name-field">
           <TextField 
           name="name" 
-          onchange = {e => onTextChange(e)} 
-          value={state.name}
+          onChange = {e => onTextChange(e)} 
+          value={comment.name}
           label="name"/>
         </div>
         <div >
           <TextField 
           name="message" 
-          onchange = {e => onTextChange(e)} 
-          value={state.message}
+          onChange = {e => onTextChange(e)} 
+          value={comment.message}
           id="outlined-multiline-static"
           label="message"/>
         </div>
@@ -57,7 +58,7 @@ function App() {
       </form>
       <div className="render-chat">
         <h1>Chat Log</h1>
-        {renderChat()}
+        {renderChatLog()}
       </div>
     </div>
   );
